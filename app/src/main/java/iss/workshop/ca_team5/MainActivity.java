9a4c85e095b88d9e315039d070fbbf70f4288c70
Delaflare <49422455+Delaflare@users.noreply.github.com>
2020-07-15 17:51:45 +0800

    music player done

--- app/src/main/java/iss/workshop/ca_team5/MainActivity.java
+++ app/src/main/java/iss/workshop/ca_team5/MainActivity.java
@@ -2,16 +2,75 @@ package iss.workshop.ca_team5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    MediaPlayer player;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //hahah
+
+        String test = "Lyra is here";
+        String test1 = "Ian left the group";
+
+//////////////////////
+        ////////////// Start play music feature////////////////////
+        player = MediaPlayer.create(this, R.raw.over_the_rainbow);
+        player.start();
+        player.setLooping(true);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true)
+                    System.out.println("Running...");
+            }
+        }).start();
+        ///////////////////end  music feature///////////
     }
-    String test = "Lyra is here";
-    String test1 = "Ian left the group";
-}
+
+
+
+
+
+
+
+
+    //////////////////////////////
+    // for background music
+        @Override
+        protected void onPause () {
+            super.onPause();
+            player.pause();
+        }
+
+        @Override
+        protected void onResume () {
+            super.onResume();
+            player.start();
+        }
+
+        @Override
+        protected void onStop () {
+            super.onStop();
+            player.seekTo(0);
+        }
+
+        @Override
+        protected void onDestroy () {
+            super.onDestroy();
+            player.release();
+        }
+
+
+    }
+//////////////////////////////
